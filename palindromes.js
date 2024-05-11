ce549c8396f90677b429f34bec29d24b0fbedf47
Brock <tammeusb@gmail.com>
2024-05-11 19:49:49 -0400

    Clean solution

--- palindromes.js
+++ palindromes.js
@@ -1,24 +1,13 @@
 const palindromes = function (string) {
-    const lowerString = string.toLowerCase();
-    console.log(lowerString);
 
-    const array = lowerString.split('');
-    console.log(array);
+    const filteredArray = string.toLowerCase().split('')
+        .filter((item) => item !== ',' && item !== ' ' && item != '!' && item != '.')
 
-    let filtered = array.filter((item) => item !== ',' && item !== ' ' && item != '!' && item != '.')
-    console.log(filtered);
+        let unreversed = filteredArray.slice().join();
+        let reversed = filteredArray.reverse().join();
 
-    let unreversed = filtered.slice();
-    console.log(unreversed);
-
-    let reversed = filtered.reverse();
-    console.log(reversed);
-
-    unreversed = unreversed.join()
-    reversed = reversed.join()
-
-    if (unreversed === reversed) { return true }
-    else { return false }
+    if (unreversed === reversed) return true 
+    else return false 
 };
 
 // Do not edit below this line
