d2423d31e6a1806625280780e3745918541cc3e2
Brock <tammeusb@gmail.com>
2024-05-11 19:46:27 -0400

    Initial solution before cleanup

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lowerString = string.toLowerCase();
+    console.log(lowerString);
 
+    const array = lowerString.split('');
+    console.log(array);
+
+    let filtered = array.filter((item) => item !== ',' && item !== ' ' && item != '!' && item != '.')
+    console.log(filtered);
+
+    let unreversed = filtered.slice();
+    console.log(unreversed);
+
+    let reversed = filtered.reverse();
+    console.log(reversed);
+
+    unreversed = unreversed.join()
+    reversed = reversed.join()
+
+    if (unreversed === reversed) { return true }
+    else { return false }
 };
 
 // Do not edit below this line
